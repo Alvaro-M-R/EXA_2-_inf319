530ed6934f110e92b4c83d921d0c01798fc03d9a
Alvaro-M-R <45154602+Alvaro-M-R@users.noreply.github.com>
2022-06-14 20:02:43 -0400

    Add files via upload

---
+++ SEGUDNO319/pregunta4/Library1.fs
@@ -0,0 +1,20 @@
+﻿namespace Library1
+module fibonaci=
+    let fib n = 
+        match n with 
+        | 0 -> 0
+        | n -> 
+         let mutable last = 0
+         let mutable next = 1
+         for i in 1 .. (n - 1) do
+            let temp = last + next
+            last <- next
+            next <- temp
+         next
+    let rec fibr n =
+        match n with
+        | 0 | 1 -> n
+        | n -> fib (n-1) + fib (n-2)
+
+type Class1() = 
+    member this.X = "F#"
